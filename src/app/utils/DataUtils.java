6750e051a72a0c84e17215b0996532f721dbd10e
KoosSA <kkposbus@gmail.com>
2021-12-02 16:55:34 +0200

    Fixed display input behind parent panel bug.

--- src/app/utils/DataUtils.java
+++ src/app/utils/DataUtils.java
@@ -1,18 +1,31 @@
 package app.utils;
 
+import java.awt.Component;
+
 import javax.swing.JOptionPane;
 
 public class DataUtils {
 	
-	public static int getDataAmount() {
-		String result = JOptionPane.showInputDialog(null, "Please enter the amount of tests to average: ");
+	public static int getDataAmount(Component parent) {
+		String result = JOptionPane.showInputDialog(parent, "Please enter the amount of tests to average: ");
 		try {
 			int size = Integer.parseInt(result);
 			return size;
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(null, "The data amount entered is not valid. Please try again.");
+			JOptionPane.showMessageDialog(parent, "The data amount entered is not valid. Please try again.");
 		}
 		return 0;
 	}
+	
+//	public static int getDataAmount() {
+//		String result = JOptionPane.showInputDialog(null, "Please enter the amount of tests to average: ");
+//		try {
+//			int size = Integer.parseInt(result);
+//			return size;
+//		} catch (Exception ex) {
+//			JOptionPane.showMessageDialog(null, "The data amount entered is not valid. Please try again.");
+//		}
+//		return 0;
+//	}
 
 }
