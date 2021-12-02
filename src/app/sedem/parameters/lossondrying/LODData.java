5131a6a5822495c8e343c10f9ff0045a7fd85cf5
KoosSA <kkposbus@gmail.com>
2021-12-02 16:52:06 +0200

    Added container weight to saved data

--- src/app/sedem/parameters/lossondrying/LODData.java
+++ src/app/sedem/parameters/lossondrying/LODData.java
@@ -7,11 +7,13 @@ public class LODData {
 	public List<Float> mass_before;
 	public List<Float> mass_after;
 	public int dataSize;
+	public List<Float> mass_container;
 	
 	public void clear() {
 		try {
 			mass_after.clear();
 			mass_before.clear();
+			mass_container.clear();
 			dataSize = 0;
 		} catch (Exception e) {
 			
