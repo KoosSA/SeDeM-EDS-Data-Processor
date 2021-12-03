98b4e92eebfda27205ff2e5c83785fe94ba1b334
KoosSA <kkposbus@gmail.com>
2021-12-03 13:48:59 +0200

    Added extra density info to adress #2.

--- src/app/sedem/parameters/density/DensityInput.java
+++ src/app/sedem/parameters/density/DensityInput.java
@@ -3,6 +3,7 @@ package app.sedem.parameters.density;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -28,12 +29,16 @@ public class DensityInput extends JFrame {
 	private JTable table;
 	private DefaultTableModel model;
 	private MainFrame main;
+	private final int COL_NUM_BULK_DENSITY = 3;
+	private final int COL_NUM_TAPPED_DENSITY = 4;
 	private final int COL_NUM_VOLUME_INITIAL = 1;
 	private final int COL_NUM_VOLUME_AFTER = 2;
 	private final int COL_NUM_MASS = 0;
 	private float tappedDensity;
 	private float bulkDensity;
 	private DensityInput instance;
+	private List<Float> bd = new ArrayList<Float>();
+	private List<Float> td = new ArrayList<Float>();
 
 	public DensityInput(MainFrame mainFrame) {
 		instance = this;
@@ -58,15 +63,21 @@ public class DensityInput extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"Weight (g)", "Initial Volume (ml)", "Volume After (ml)"
+				"Weight (g)", "Initial Volume (ml)", "Volume After (ml)", "Bulk Density (g/ml)", "Tapped Density (g/ml)"
 			}
 		) {
 			private static final long serialVersionUID = -2335450417166994732L;
 			Class<?>[] columnTypes = new Class[] {
-				Float.class, Float.class, Float.class
+				Float.class, Float.class, Float.class, Float.class, Float.class
 			};
 			public Class<?> getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				true, true, true, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
 			}
 		});
 		table.getColumnModel().getColumn(1).setPreferredWidth(97);
@@ -102,6 +113,14 @@ public class DensityInput extends JFrame {
 		panel.add(btnNewButton);
 		panel.add(btn_addData);
 		
+		JButton btn_calc = new JButton("Calculate");
+		btn_calc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				calculate();
+			}
+		});
+		panel.add(btn_calc);
+		
 		JButton btn_save = new JButton("Save");
 		btn_save.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -119,20 +138,42 @@ public class DensityInput extends JFrame {
 		data.volume_before = TableUtils.getAllValuesInColumn(table, COL_NUM_VOLUME_INITIAL);
 		data.mass = TableUtils.getAllValuesInColumn(table, COL_NUM_MASS);
 		data.dataSize = table.getRowCount();
-		bulkDensity = calculate(data.mass, data.volume_before);
-		tappedDensity = calculate(data.mass, data.volume_after);
+//		bulkDensity = calculate(data.mass, data.volume_before);
+//		tappedDensity = calculate(data.mass, data.volume_after);
+		calculate();
 		main.setDensityData();
 		setVisible(false);
 	}
 
-	private float calculate(List<Float> mass, List<Float> volume) {
+	private void calculate() {
+		tappedDensity = 0;
+		bulkDensity = 0;
+		bd.clear();
+		td.clear();
+		List<Float> m = TableUtils.getAllValuesInColumn(table, COL_NUM_MASS);
+		List<Float> vi = TableUtils.getAllValuesInColumn(table, COL_NUM_VOLUME_INITIAL);
+		List<Float> vf = TableUtils.getAllValuesInColumn(table, COL_NUM_VOLUME_AFTER);
+		for (int i = 0; i < table.getRowCount(); i++) {
+			bd.add(m.get(i) / vi.get(i));
+			td.add(m.get(i) / vf.get(i));
+			tappedDensity += td.get(i);
+			bulkDensity += bd.get(i);
+		}
+		TableUtils.setAllValuesInColumn(table, COL_NUM_BULK_DENSITY, bd);
+		TableUtils.setAllValuesInColumn(table, COL_NUM_TAPPED_DENSITY, td);
+		tappedDensity /= table.getRowCount();
+		bulkDensity /= table.getRowCount();
+		
+	}
+
+	/*private float calculate(List<Float> mass, List<Float> volume) {
 		float d = 0;
 		for (int i = 0; i < table.getRowCount(); i++) {
 			d += mass.get(i) / volume.get(i);
 		}
 		d /= table.getRowCount();
 		return d;
-	}
+	}*/
 
 	public DensityData getData() {
 		return data;
@@ -160,8 +201,9 @@ public class DensityInput extends JFrame {
 		TableUtils.setAllValuesInColumn(table, COL_NUM_MASS, data.mass);
 		TableUtils.setAllValuesInColumn(table, COL_NUM_VOLUME_AFTER, data.volume_after);
 		TableUtils.setAllValuesInColumn(table, COL_NUM_VOLUME_INITIAL, data.volume_before);
-		bulkDensity = calculate(data.mass, data.volume_before);
-		tappedDensity = calculate(data.mass, data.volume_after);
+//		bulkDensity = calculate(data.mass, data.volume_before);
+//		tappedDensity = calculate(data.mass, data.volume_after);
+		calculate();
 	}
 	
 	private void clearAllData(boolean skip) {
